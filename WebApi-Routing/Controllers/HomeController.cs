ef14ec0b197817aa6202f9cd1441bff5d30b35fc
wissenfsd@gmail.com <wissenfsd@gmail.com>
2024-04-21 12:39:03 +0300

    sdsd

--- WebApi-Routing/Controllers/HomeController.cs
+++ WebApi-Routing/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace WebApi_Routing.Controllers
 
     public class HomeController : ControllerBase
     {
-        [HttpGet]
+        //[HttpGet]
         // Ortamda sadexe tek bir get varsa, bu apiyi çalıştırırken, metot adı vermenize gerek kalmaz.
         // Bu sadece get için değil, diğer tüm action'lar için geçerlidir. Post-Put-Delete-Get
         //public IActionResult SiparisGetir()
@@ -28,58 +28,80 @@ namespace WebApi_Routing.Controllers
 
 
         // Bu metodun route attribute ile 
-        [Route("api/getir")]
-        public IActionResult SiparisGetir()
-        {
-            return Ok("Sipariş geldi");
-        }
-        [HttpGet]
-
+        //[Route("api/getir")]
+        //public IActionResult SiparisGetir()
+        //{
+        //    return Ok("Sipariş geldi");
+        //}
+        //[HttpGet]
         //Routing'de, tilda kullanıyorsanız, Controllerden gelen routing özelliğini almayacaktır. ©®§
-        [Route("~/api/urun/getir")]
-        public IActionResult UrunleriGetir()
-        {
-            return Ok();
-        }
+        //[Route("~/api/urun/getir")]
+        //public IActionResult UrunleriGetir()
+        //{
+        //    return Ok();
+        //}
 
         // Parametreye göre routing verme(uygulama çalıştırıldığında, parametre routing olarak görünecektir.)
 
-        [HttpGet]
-        [Route("{wissen}")]
-        public IActionResult ParametreyeGoreIsim(string wissen)
-        {
-            return Ok();
-        }
+        //[HttpGet]
+        //[Route("{wissen}")]
+        //public IActionResult ParametreyeGoreIsim(string wissen)
+        //{
+        //    return Ok();
+        //}
 
         // Parametreye göre kilit koyma için kullanılır.
         // Göndeirlecek parametreyi routing ??
 
-        //?
-        // 1 : parametre bu olmalıdır.
-        // 2. parametre vermezsen, Wissen Beşiktaş parametre olacaktır.
-        [HttpGet]
-        [Route("veri/sayfa/{wissen=Wissen Beşiktaş}")]
-        public IActionResult ParametreVerisineGoreIsim(string wissen)
-        {
 
-            return Ok();
-        }
+      
+        // 1. parametre vermezsen, Wissen Beşiktaş parametre olacaktır.
+        //[HttpGet]
+        //[Route("veri/sayfa/{wissen=Wissen Besiktas}")]
+        //public IActionResult ParametreVerisineGoreIsim(string wissen)
+        //{
 
-        //?
+        //    return Ok();
+        //}
+
+
         [HttpGet]
-        [Route("{veri?}")]
-        public IActionResult NullOlabilir(string veri)
+        [Route("{peugeot?}")]
+        public IActionResult NullOlabilir(string peugeot=null)
         {
             return Ok();
         }
 
         //Int tipinde deger parametresi alacağını routing olarak belirlemek 
-        [HttpGet]
-        [Route("data/{deger:int}")]
-        public IActionResult IntParametre(int deger)
-        {
-            return Ok();
-        }
+        //[HttpGet]
+        //[Route("data/{deger:int}")]
+        //public IActionResult IntParametre(int deger)
+        //{
+        //    return Ok();
+        //}
+
+        // Girilen id parametresi 5'in altında bir değer olamaz
+        //[HttpGet]
+        //[Route("{id:int:min(5)}")]
+        //public IActionResult MinParametre(int id)
+        //{
+        //    return Ok();
+        //}
+
+        // Girilen parametre minimum 5 maximum 15 olmalıdır.
+        //[HttpGet]
+        //[Route("{veri:int:min(5):max(15)}")]
+        //public IActionResult MinMaxParametre(int veri)
+        //{
+        //    return Ok();
+        //}
+
+        //[HttpGet]
+        //[Route("{data:int:range(1,3)}")]
+        //public IActionResult Range(int data)
+        //{
+        //    return Ok();
+        //}
 
 
     }
